9796e232877730a6962783eb79537f3aaa8a3627
yousef2468-1 <148258330+yousef2468-1@users.noreply.github.com>
2024-05-16 22:08:37 +0300

    Update urls.py

--- Library_Website/project/pages/urls.py
+++ Library_Website/project/pages/urls.py
@@ -2,17 +2,22 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('index.html', views.index, name='index'),
-    path('index_books', views.index_books, name='index_books'),
+    path('', views.index, name='index'),  # URL pattern for the homepage
+    path('index_books/', views.index_books, name='index_books'),
     path('Template_book/<str:title>/', views.Template_book, name='Template_book'),
     path('Template_book_details/<str:title>/', views.Template_book_details, name='Template_book_details'),
-
-    path('categories.html', views.categories, name='categories'),
-    path('Login.html', views.Login, name='Login'),
-    path('Signup.html', views.Signup, name='Signup'),
-    path('Template_Category.html', views.Template_Category, name='Template_Category'),
-    path('Not_Available.html', views.Not_Available, name='Not_Available'),
-    path('confirm.html', views.confirm, name='confirm'),
-    path('Borrow_book.html', views.Borrow_book, name='Borrow_book'),
-  
+    path('categories/', views.categories, name='categories'),
+    path('Login/', views.Login, name='Login'),
+    path('Signup/', views.Signup, name='Signup'),
+    path('Template_Category/', views.Template_Category, name='Template_Category'),
+    path('Not_Available/', views.Not_Available, name='Not_Available'),
+    path('confirm/', views.confirm, name='confirm'),
+    path('Borrow_book/', views.Borrow_book, name='Borrow_book'),
+    path('index.html', views.index, name='index_html'), 
+      path('categories.html', views.categories, name='categories'), 
+      path('Login.html', views.Login, name='Login'),
+      path('Signup.html', views.Signup, name='Signup'),  
+      path('Borrow_book.html', views.Borrow_book, name='Borrow_book'),
+       
+        # URL pattern for index.html
 ]
