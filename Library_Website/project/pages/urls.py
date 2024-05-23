aa06adb495dcfe5aac79d39357b809c7c4a66d3b
yousef2468-1 <148258330+yousef2468-1@users.noreply.github.com>
2024-05-24 01:29:55 +0300

    Update urls.py

--- Library_Website/project/pages/urls.py
+++ Library_Website/project/pages/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from . import views
-
+from .views import admin_users, delete_user
 urlpatterns = [
     path('', views.index, name='index'),  # URL pattern for the homepage
     path('index_books/', views.index_books, name='index_books'),
@@ -25,4 +25,6 @@ urlpatterns = [
     path('admin_books', views.admin_books, name='admin_books'),
     path('admin_login', views.admin_login, name='admin_login'),
     path('delete_book/<int:book_id>/', views.delete_book, name='delete_book'),
+      path('admin-users/', admin_users, name='admin_users'),
+        path('delete-user/<int:user_id>/', delete_user, name='delete_user'),
 ]
